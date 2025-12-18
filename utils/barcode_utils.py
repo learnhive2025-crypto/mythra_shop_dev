8b7e6ad4c19af3a5c1947f190bffbde803fad942
learnhive <your_email@example.com>
2025-12-18 17:56:37 +0530

    main

--- utils/barcode_utils.py
+++ utils/barcode_utils.py
@@ -2,4 +2,4 @@
 import random
 
 def generate_barcode():
-    return "89" + "".join(str(random.randint(0, 9)) for _ in range(11))
+    return "01" + "".join(str(random.randint(0, 9)) for _ in range(2))
